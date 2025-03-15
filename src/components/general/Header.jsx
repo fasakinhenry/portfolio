35f1daf884f02b5a3d9770955db9d2bb5665fdfe
fisayo-dev <olufisayobadina@gmail.com>
2025-03-15 20:43:49 +0100

    style: quick design of header component

--- src/components/general/Header.jsx
+++ src/components/general/Header.jsx
@@ -1,8 +1,37 @@
-const Header = () => {
-    return (
-      // Your header content here
-    <div>Header</div>
-  )
-}
+import { Link } from "react-router-dom";
 
-export default Header
+const Header = () => {
+  const links = [
+    "home",
+    "skills",
+    "experience",
+    "projects",
+    "socials",
+    "contact",
+  ];
+  return (
+    // Your header content here
+    <div className="bg-blue-500">
+      <div className="container mx-auto py-4 flex items-center justify-between">
+        <h2>Fasakin Henry</h2>
+        {/* Navigation links */}
+        <nav>
+          <ul className="flex space-x-4">
+            {links.map((link) => (
+              <li key={link}>
+                <Link
+                  to={`#${link}`}
+                  className="capitalize text-white hover:underline"
+                >
+                  {link}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
