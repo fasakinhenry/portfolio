28f845114742990d6db3b63f361d526459f2055b
fisayo-dev <olufisayobadina@gmail.com>
2025-03-15 19:20:52 +0100

    chore: write into geenral component files

---
+++ src/components/general/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+    //   Your footer content here
+    <div>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/general/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+      // Your header content here
+    <div>Header</div>
+  )
+}
+
+export default Header
