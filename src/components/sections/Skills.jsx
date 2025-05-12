a0741675512755e9583e4fc1d08eee6dc3624573
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-12 14:37:10 +0100

    style: update mobile look of skills sections

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -26,31 +26,31 @@ const Skills = () => {
   ];
 
   return (
-    <section className="w-full flex justify-center items-center px-4 py-10">
-      <div className="w-full max-w-[53rem] flex flex-col items-center relative bg-black px-[1rem] md:px-[5rem] py-[5rem] rounded-xl">
+    <section className='w-screen px-4 py-10 flex justify-center items-center'>
+      <div className='w-full max-w-[53rem] flex flex-col items-center relative bg-black px-4 md:px-[5rem] py-[5rem] rounded-xl'>
         <Image
-          alt="Doodle"
-          loading="lazy"
-          decoding="async"
-          src="/doodle.png"
-          layout="fill"
-          objectFit="cover"
-          className="absolute top-0 left-0 opacity-10"
+          alt='Doodle'
+          loading='lazy'
+          decoding='async'
+          src='/doodle.png'
+          layout='fill'
+          objectFit='cover'
+          className='absolute top-0 left-0 opacity-10'
         />
-        <h2 className="text-[32px] font-bold tracking-[-.03em] leading-[110%] text-white text-center w-full mb-[16px]">
+        <h2 className='text-[32px] font-bold tracking-[-.03em] leading-[110%] text-white text-center w-full mb-[16px]'>
           How can I help?
         </h2>
-        <p className="text-[18px] text-[#a4a2a2] text-center w-full mb-[40px]">
+        <p className='text-[18px] text-[#a4a2a2] text-center w-full mb-[40px]'>
           Let's turn your vision into something amazing.
         </p>
-        <div className="flex gap-6 w-full">
+        <div className='flex gap-6 w-full'>
           <Marquee gradient={false} speed={50} pauseOnHover={true}>
             {skills.map((skill, index) => (
               <div
                 key={index}
-                className="bg-[#F9F9F9] mr-5 rounded-[16px] px-6 py-3 flex flex-col hover:scale-105 duration-300 items-center"
+                className='bg-[#F9F9F9] mr-5 rounded-[16px] px-6 py-3 flex flex-col hover:scale-105 duration-300 items-center'
               >
-                <h3 className="text-[13px] font-medium">{skill}</h3>
+                <h3 className='text-[13px] font-medium'>{skill}</h3>
               </div>
             ))}
           </Marquee>
