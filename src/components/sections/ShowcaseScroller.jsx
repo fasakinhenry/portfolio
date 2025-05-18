883bc797493f92f58384b3f8ac5a47fa6a75df90
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-18 05:53:02 +0100

    style: update the styling of the showcase component

--- src/components/sections/ShowcaseScroller.jsx
+++ src/components/sections/ShowcaseScroller.jsx
@@ -1,30 +1,15 @@
 'use client';
 
 import Marquee from 'react-fast-marquee';
+import projects from '@/data/projects';
 
 const ShowcaseScroller = () => {
-  const items = [
-    { src: '/showcase/careproxy.png', alt: 'CareProxy' },
-    { src: '/payforeign.png', alt: 'PayForeign' },
-    { src: '/ondepay.png', alt: 'Ondepay Virtual Card V2' },
-    // Add more items as needed
-  ];
+  const cardShadow = 'rgb(255, 255, 255) 0px 3px 0px 0px inset';
+  const innerShadow = 'rgba(0, 0, 0, 0.25) 0px 10px 16px -3px';
+  const cornerDotShadow =
+    'rgba(0, 0, 0, 0.1) 0px 1px 1px 0px inset, rgb(255, 255, 255) 0px -0.5px 0px 0px inset';
 
-  const cardStyles = {
-    boxShadow: 'rgb(255, 255, 255) 0px 3px 0px 0px inset',
-    transition: 'transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
-    willChange: 'transform',
-    transform: 'translateZ(0)',
-  };
-
-  const innerShadow = {
-    boxShadow: 'rgba(0, 0, 0, 0.25) 0px 10px 16px -3px',
-  };
-
-  const cornerDotShadow = {
-    boxShadow:
-      'rgba(0,0,0,0.1) 0px 1px 1px 0px inset, rgb(255,255,255) 0px -0.5px 0px 0px inset',
-  };
+  const duplicatedProjects = [...projects, ...projects];
 
   return (
     <div className='w-full overflow-hidden transform-none py-16'>
@@ -36,52 +21,54 @@ const ShowcaseScroller = () => {
         direction='left'
         pauseOnHover={false}
       >
-        {[...items, ...items].map((item, index) => (
+        {duplicatedProjects.map((project, index) => (
           <div
             key={index}
-            className='mx-6'
-            style={{
-              perspective: '1000px',
-              transformStyle: 'preserve-3d',
-            }}
+            className='rfm-child'
+            style={{ '--transform': 'none' }}
           >
-            <div
-              className='flex w-[400px] h-[280px] rounded-[20px] overflow-hidden border border-[#f2f2f2] bg-[#fafafa] relative hover:scale-[1.05]'
-              style={cardStyles}
-            >
+            <div className='flex px-4 md:px-3'>
               <div
-                className='p-6 flex items-center justify-center w-full h-full rounded-[10px]'
-                style={innerShadow}
+                className='flex min-w-[400px] md:min-w-[400px] w-full rounded-[20px] overflow-hidden border-[1.5px] border-[#f2f2f2] bg-[#fafafa] relative transition-all hover:scale-[1.02]'
+                style={{ boxShadow: cardShadow }}
               >
-                <img
-                  alt={item.alt}
-                  src={item.src}
-                  draggable={false}
-                  loading='lazy'
-                  decoding='async'
-                  className='rounded-[10px]'
-                  style={{
-                    width: '300px',
-                    height: '200px',
-                    objectFit: 'cover',
-                    flexShrink: 0,
-                  }}
-                />
-              </div>
-
-              {/* Corner dots */}
-              {[
-                'top-4 left-4',
-                'bottom-4 left-4',
-                'top-4 right-4',
-                'bottom-4 right-4',
-              ].map((pos, i) => (
                 <div
-                  key={i}
-                  className={`absolute w-2 h-2 ${pos} rounded-full bg-[#e6e6e6]`}
-                  style={cornerDotShadow}
-                />
-              ))}
+                  className='p-8 rounded-[10px]'
+                  style={{ boxShadow: innerShadow }}
+                >
+                  <div
+                    className='relative'
+                    style={{
+                      filter: 'drop-shadow(rgba(0, 0, 0, 0.1) 0px 10px 7px)',
+                    }}
+                  >
+                    <img
+                      alt={project.title}
+                      draggable={false}
+                      loading='lazy'
+                      width={400}
+                      height={400}
+                      decoding='async'
+                      className='w-full h-[250px] object-cover rounded-[10px]'
+                      src={project.image}
+                    />
+                  </div>
+                </div>
+
+                {/* Corner dots */}
+                {[
+                  'top-4 left-4',
+                  'bottom-4 left-4',
+                  'top-4 right-4',
+                  'bottom-4 right-4',
+                ].map((pos, i) => (
+                  <div
+                    key={i}
+                    className={`absolute w-2 h-2 ${pos} rounded-full bg-[#e6e6e6]`}
+                    style={{ boxShadow: cornerDotShadow }}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         ))}
