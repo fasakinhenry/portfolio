6a93790934b81960002f44b61500c2a779104b53
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-24 21:33:00 +0100

    style(resume-page): improved responsiveness on mobile

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -10,7 +10,7 @@ export default function ResumePage() {
     <main className='min-h-screen bg-white text-black'>
       <Navbar />
       <VerticalBoundaries />
-      <div className='flex flex-col items-center w-full mt-36'>
+      <div className='flex flex-col items-center w-full mt-36 px-6'>
         <div className='w-full max-w-[53rem]'>
           <h1 className='text-4xl font-bold mb-1 text-center'>Resumes</h1>
           <p className='text-center text-[#5a5a5a] mb-8'>
